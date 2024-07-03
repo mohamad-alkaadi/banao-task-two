19b788dbd0daf655593cc4f40c34ecd91c18ee70
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2024-07-03 18:07:08 +0400

    bad request

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter"
 import UsersList from "../components/UsersList"
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -36,14 +37,20 @@ export default function RootLayout({
                 sx={{
                   height: "40px",
                   display: "flex",
-                  pl: 2,
+                  px: 2,
                   fontSize: "23px",
                   fontWeight: 500,
-                  justifyContent: "start",
+                  justifyContent: "space-between",
                   alignItems: "center",
                 }}
               >
                 Users
+                <Link
+                  className="border-[1px] border-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:border-0 hover:to-pink-500 text-[13px] px-2 py-1"
+                  href={`/11223344}`}
+                >
+                  Bad Request
+                </Link>
               </Box>
               <UsersList />
             </Grid>
